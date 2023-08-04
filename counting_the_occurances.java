f86f9c6a4df6179ac9a713e7cce3f7b98e8a59e7
Yaswanthjangila <119721154+Yaswanthjangila@users.noreply.github.com>
2023-08-04 22:28:47 +0530

    import java.util.Scanner; public class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         char ch=sc.next().charAt(0);         int c=0;         for(int i=0;i<s.length();i++)         {             if(s.charAt(i)==ch)                c++;         }         if(c==0)         {             System.out.println(-1);         }         else         {             System.out.println(c);         }     } }

---
+++ counting_the_occurances.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        char ch=sc.next().charAt(0);
+        int c=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)==ch)
+               c++;
+        }
+        if(c==0)
+        {
+            System.out.println(-1);
+        }
+        else
+        {
+            System.out.println(c);
+        }
+    }
+}
